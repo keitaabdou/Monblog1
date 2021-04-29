90f92bc00218639a457e2cb93dcb28bf222f6d57
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-29 21:01:16 +0000

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,69 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Page
+            </h3>
+
+            <ul class="py-4 sm:text-5 pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">Accueil</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">Blog</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">Connexion</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">S'inscrire</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Nous trouvez
+            </h3>
+
+            <ul class="py-4 sm:text-5 pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">Ce que nous faisons</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">Adresse</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">Téléphone</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Derniers messages
+            </h3>
+
+            <ul class="py-4 sm:text-5 pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">Pourquoi nous aimons la technologie</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">Pourquoi nous aimons le design</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">Pourquoi utiliser laravel</a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">Pourquoi PHP est le meilleur</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2021 Code avec keita. Tous les droits sont réservés
+    </p>
+</footer>
